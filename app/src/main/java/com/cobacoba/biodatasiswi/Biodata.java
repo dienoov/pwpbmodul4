7afaf12d51ef81f8b3a4fcc629075e8b38ad364f
dienoov <alvinodien@yahoo.co.id>
2021-01-14 16:11:52 +0700

    tampil biodiesel

--- app/src/main/java/com/cobacoba/biodatasiswi/Biodata.java
+++ app/src/main/java/com/cobacoba/biodatasiswi/Biodata.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Biodata extends AppCompatActivity {
 
@@ -14,8 +18,32 @@ public class Biodata extends AppCompatActivity {
         setContentView(R.layout.activity_biodata);
     }
 
-    public void pindadActivity(View view){
+    public void pindadActivity(View view) {
         Intent intent = new Intent(Biodata.this, Tampilin.class);
+
+        EditText nama = findViewById(R.id.nama);
+        intent.putExtra("nama", nama.getText().toString());
+
+        RadioGroup jenisGender = findViewById(R.id.jenisGender);
+        RadioButton kepilihGendre = findViewById(jenisGender.getCheckedRadioButtonId());
+        intent.putExtra("gender", kepilihGendre.getText().toString());
+
+        CheckBox baca = findViewById(R.id.baca);
+        CheckBox olah = findViewById(R.id.olah);
+        CheckBox kuli = findViewById(R.id.kuli);
+        String lobi = "";
+
+        if (baca.isChecked())
+            lobi += baca.getText().toString() + ", ";
+
+        if (olah.isChecked())
+            lobi += olah.getText().toString() + ", ";
+
+        if (kuli.isChecked())
+            lobi += kuli.getText().toString() + ", ";
+
+        intent.putExtra("bobi", lobi);
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/cobacoba/biodatasiswi/Tampilin.java
+++ app/src/main/java/com/cobacoba/biodatasiswi/Tampilin.java
@@ -3,6 +3,7 @@ package com.cobacoba.biodatasiswi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tampilin extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class Tampilin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilin);
+
+        String nama = getIntent().getExtras().getString("nama");
+        TextView tampilNama = findViewById(R.id.tampilNama);
+
+        String gender = getIntent().getExtras().getString("gender");
+        TextView tampilGender = findViewById(R.id.tampilGender);
+
+        String bobi = getIntent().getExtras().getString("bobi");
+        TextView tampilBobi = findViewById(R.id.tampilBobi);
+
+        tampilNama.setText("Nama: " + nama);
+        tampilGender.setText("Jenis: " + gender);
+        tampilBobi.setText("Bobi: " + bobi);
     }
 }
